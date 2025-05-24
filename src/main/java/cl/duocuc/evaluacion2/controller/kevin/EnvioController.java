fd2b20c362655918a080ba08cdd5006c98b4755f
Kmorales212 <ke.moralese@duocuc.cl>
2025-05-24 04:59:50 -0400

    se crea EnvioController

---
+++ src/main/java/cl/duocuc/evaluacion2/controller/kevin/EnvioController.java
@@ -0,0 +1,50 @@
+package cl.duocuc.evaluacion2.controller.kevin;
+
+import cl.duocuc.evaluacion2.model.EnvioModel;
+import cl.duocuc.evaluacion2.service.EnvioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/envios")
+public class EnvioController {
+
+    @Autowired
+    private EnvioService envioService;
+
+    @GetMapping
+    public List<EnvioModel> getAllEnvios() {
+        return envioService.getAllEnvios();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<EnvioModel> getEnvioById(@PathVariable String id) {
+        return envioService.getEnvioById(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public EnvioModel createEnvio(@RequestBody EnvioModel envio) {
+        return envioService.createEnvio(envio);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<EnvioModel> updateEnvio(@PathVariable String id, @RequestBody EnvioModel envioDetails) {
+        return envioService.updateEnvio(id, envioDetails)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteEnvio(@PathVariable String id) {
+        boolean deleted = envioService.deleteEnvio(id);
+        if (!deleted) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.noContent().build();
+    }
+}
