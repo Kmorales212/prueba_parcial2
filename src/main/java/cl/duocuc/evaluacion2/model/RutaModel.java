f521b35d9147d4781088f186321429fe2628891b
Kmorales212 <ke.moralese@duocuc.cl>
2025-05-28 14:36:04 -0400

    se corrigen errores

--- src/main/java/cl/duocuc/evaluacion2/model/RutaModel.java
+++ src/main/java/cl/duocuc/evaluacion2/model/RutaModel.java
@@ -5,31 +5,39 @@ import lombok.Data;
 
 import java.time.LocalDateTime;
 import java.util.List;
+
 @Data
 @Entity
+@Table(name = "ruta")
 public class RutaModel {
 
-   @Id
-   private String idRuta;
+    @Id
+    @Column(name = "id_ruta")
+    private String idRuta;
 
-   private LocalDateTime fechaInicio;
+    @Column(name = "fecha_inicio")
+    private LocalDateTime fechaInicio;
 
-   @OneToMany(cascade = CascadeType.ALL)
-   private List<EnvioModelo> envios;
-
-   @OneToOne(cascade = CascadeType.ALL)
-   private DireccionModelo direccionInicio;
+    @OneToMany(mappedBy = "ruta", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<EnvioModelo> envios;
 
     @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_direccion_inicio")
+    private DireccionModelo direccionInicio;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_direccion_destino")
     private DireccionModelo direccionDestino;
 
     @ManyToOne
+    @JoinColumn(name = "id_ciudad")
     private CiudadModelo ciudad;
 
     @Enumerated(EnumType.STRING)
+    @Column(name = "estado")
     private EstadoRuta estado;
 
+    @Column(name = "descripcion")
     private String descripcion;
-
 
 }
