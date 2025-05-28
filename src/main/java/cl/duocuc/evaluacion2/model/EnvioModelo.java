542e2280a475f424dc9ad59eb0301083e030b383
Kmorales212 <ke.moralese@duocuc.cl>
2025-05-28 14:34:35 -0400

    se corrigen errores

--- src/main/java/cl/duocuc/evaluacion2/model/EnvioModelo.java
+++ src/main/java/cl/duocuc/evaluacion2/model/EnvioModelo.java
@@ -5,22 +5,29 @@ import jakarta.persistence.*;
 import lombok.Data;
 
 import java.time.LocalDate;
+
 @Data
 @Entity
+@Table(name = "envio")
 public class EnvioModelo {
 
-@Id
+    @Id
+    @Column(name = "id_envio")
     private String idEnvio;
+
+    @Column(name = "fecha_envio")
     private LocalDate fechaEnvio;
 
-
     @Enumerated(EnumType.STRING)
+    @Column(name = "estado")
     private EstadoEnvio estado;
 
     @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_direccion")
     private DireccionModelo direccionEntrega;
 
     @ManyToOne
     @JoinColumn(name = "id_ruta")
     private RutaModel ruta;
+
 }
