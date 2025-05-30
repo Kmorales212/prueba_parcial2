fdb87fbad472aca23d209a2b35cb5d557264154a
TasticVal <val.nunezo@duocuc.cl>
2025-05-30 01:38:28 -0400

    creacion de ProductoModelo

---
+++ src/main/java/cl/duocuc/evaluacion2/model/ProductoModelo.java
@@ -0,0 +1,17 @@
+package cl.duocuc.evaluacion2.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class ProductoModelo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String descripcion;
+    private Double precio;
+}
