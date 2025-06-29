c986e9316c089222d2c55eef4861fb7811a0af7b
TasticVal <val.nunezo@duocuc.cl>
2025-06-29 00:38:12 -0400

    se le agrega la documentacion del swagger

--- src/main/java/cl/duocuc/evaluacion2/controller/valeria/ProductoController.java
+++ src/main/java/cl/duocuc/evaluacion2/controller/valeria/ProductoController.java
@@ -3,6 +3,8 @@ package cl.duocuc.evaluacion2.controller.valeria;
 import cl.duocuc.evaluacion2.dto.ProductoDTO;
 import cl.duocuc.evaluacion2.model.ProductoModelo;
 import cl.duocuc.evaluacion2.service.ProductoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +14,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/productos")
+@Tag(name = "Gestion de Productos")
 public class ProductoController {
 
     @Autowired
@@ -35,14 +38,14 @@ public class ProductoController {
         return producto;
     }
 
-
+    @Operation(summary = "crear producto", description = "este metodo crea un producto nuevo")
     @PostMapping("/crear")
     public ResponseEntity<ProductoDTO> crear(@RequestBody ProductoDTO dto) {
         ProductoModelo producto = productoService.crearProducto(toEntity(dto));
         return ResponseEntity.ok(toDTO(producto));
     }
 
-
+    @Operation(summary = "listar todos los productos", description = "este metodo se encarga de listar todos los productos existentes en nuestra base de datos")
     @GetMapping("/listar")
     public ResponseEntity<List<ProductoDTO>> listarTodos() {
         List<ProductoDTO> productos = productoService.obtenerTodos()
@@ -53,6 +56,7 @@ public class ProductoController {
     }
 
 
+    @Operation(summary = "buscar por id", description = "este metodo se encarga de buscar los datos de un producto mediante su id")
     @GetMapping("/buscar/{id}")
     public ResponseEntity<ProductoDTO> buscarPorId(@PathVariable Long id) {
         return productoService.obtenerPorId(id)
@@ -61,7 +65,22 @@ public class ProductoController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
+    @Operation(summary = "actualizar por id", description = "este metodo se encarga de buscar un producto por su id para luego poder actualizar algun dato de el mismo")
+    @PutMapping("/actualizar/{id}")
+    public ResponseEntity<ProductoDTO> actualizar(@PathVariable Long id, @RequestBody ProductoDTO dto) {
+        return productoService.obtenerPorId(id).map(productoExistente -> {
+            productoExistente.setNombre(dto.getNombre());
+            productoExistente.setDescripcion(dto.getDescripcion());
+            productoExistente.setPrecio(dto.getPrecio());
 
+            ProductoModelo actualizado = productoService.crearProducto(productoExistente);
+            return ResponseEntity.ok(toDTO(actualizado));
+        }).orElse(ResponseEntity.notFound().build());
+    }
+
+
+
+    @Operation(summary = "eliminar por id", description = "este metodo se encarga de eliminar un producto mediante su id")
     @DeleteMapping("/eliminar/{id}")
     public ResponseEntity<Void> eliminar(@PathVariable Long id) {
         productoService.eliminarPorId(id);
